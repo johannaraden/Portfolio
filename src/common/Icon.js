6b072e3d7ab29baea61e1c376791a516bbd42727
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-05-17 22:52:05 +0200

    Updated to style component

--- src/common/Icon.js
+++ src/common/Icon.js
@@ -8,20 +8,7 @@ const Circle = styled.img`
   margin: 1em 1.1em;
 `
 const Link = styled.a`
-  position: relative;
-  :before {
-    content: '';
-    background: #FFADAD;
-    position: absolute;
-    width: 0%;
-    top: 0;
-    left: 0;
-    height: 100%;
-    transition: 0.3s linear;
-    z-index: -1;
-  }
-  :hover:before {
-    width: 100%;
+ 
 `
 
 export const Icon = ({ src, href }) => {
