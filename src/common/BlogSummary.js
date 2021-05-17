94442914caf3cee6f7f2e17c855350ec1a7ed930
johannaraden <51335476+johannaraden@users.noreply.github.com>
2021-05-17 14:28:39 +0200

    testing

--- src/common/BlogSummary.js
+++ src/common/BlogSummary.js
@@ -8,7 +8,7 @@ const CardContainer = styled.div`
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0,0,0,.12);
   display: flex;
   flex-direction: row; 
-  background-color: white;
+  background-color: black;
   box-sizing: border-box;
   margin: 1em;
   padding: 2em;
